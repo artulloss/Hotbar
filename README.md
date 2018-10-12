61e6d1cdac3de8e2a4dd5855cd861269ca3a23f6
artulloss <35859316+artulloss@users.noreply.github.com>
2018-10-12 19:52:16 -0400

    lotta updates

--- README.md
+++ README.md
@@ -1,11 +1,114 @@
 [![HitCount](http://hits.dwyl.io/artulloss/Hotbar.svg)](http://hits.dwyl.io/artulloss/Hotbar)
 # Hotbar
 A simple hotbar plugin!
+#### Configuration
 
-Each line in the slots goes like this:
+The configuration is pretty simple
+```
+---
+Items:
+  world:
+  - Item: "399:0:1"
+    ItemName: Name
+    Command: Command
+    Executor: Player
+    Enchant: true
+Locked Inventory: []
+Cooldown: 0.500000
+...
+```
 
-world,slot(0 is first slot),itemID:itemMeta,nameOfItem,command,executeAsPlayerOrConsole,enchant(set to "true" for enchant)
+While most are pretty self explanatory, please note that Executor who is executing the command, either the player or the console
 
-If you execute as console you can do {PLAYER} to get the player who tapped the item.
+If you use console you can do {PLAYER} to refer to the player
 
-The cooldown is to "fix" the player interact spam, I recommend the default value, but you can set it as high/low as you'd like.
+You can add as many items and worlds as you'd like!
+```
+---
+Items:
+  FirstWorld:
+  - Item: "399:0:1"
+    ItemName: Name
+    Command: Command
+    Executor: Player
+    Enchant: true
+  SecondWorld:
+  - Item: "339:0:1"
+    ItemName: Name
+    Command: Command
+    Executor: Player
+    Enchant: true
+  - Item: "339:0:1"
+    ItemName: Name
+    Command: Command
+    Executor: Player
+    Enchant: true
+Locked Inventory: []
+Cooldown: 0.500000
+...
+```
+#### Duplicate items
+
+You can deal with duplicate items by using different meta values for them or different counts.
+
+Duplicate items are only an issue if on the same world
+
+In the example above, there isn't a conflict between 399:0:1 in the first world and the second world
+
+However there is a conflict between the two 399:0:1's in the second world
+
+Here is an example (The other parts are gone to save space)
+```
+SecondWorld:
+  - Item: "339:0:1" # The first item
+  - Item: "339:0:1" # The second one, this is invalid
+  - Item: "339:1:1" # The second one, in a valid way
+  - Item: "339:0:2" # The second one, also a valid way
+  ```
+
+**By Default items go in order of placement in config however you can change that! Remember, arrays start at 0, and so do item slots!**
+
+```
+---
+---
+Items:
+  FirstWorld:
+  - Item: '339:0:1'
+    ItemName: Name
+    Command: Command
+    Executor: Player
+    Enchant: true
+  SecondWorld:
+    '0':
+      Item: '339:0:1'
+      ItemName: Name
+      Command: Command
+      Executor: Player
+      Enchant: true
+    '8':
+      Item: '339:1:1'
+      ItemName: Name
+      Command: Command
+      Executor: Player
+      Enchant: true
+Locked Inventory: []
+  Cooldown: 0.5
+---
+```
+
+#### Locking Inventories
+
+You may wish to block a player from modifying the hotbar, the "Locked Inventory" part of the config allows for this:
+
+```
+Locked Inventory:
+- example
+- as  many as you want
+```
+
+#### Cooldown
+The cooldown is to "fix" the player interact spam, which is a client side bug with Windows 10, I recommend the default value, but you can set it as high/low as you'd like.
+
+#### Support?
+Tweet me [@artulloss](https://twitter.com/artulloss)
+Discord [here](https://discord.versai.pro)
--- src/Hotbar/Hotbar.php
+++ src/Hotbar/Hotbar.php
@@ -8,9 +8,15 @@
 declare(strict_types = 1);
 namespace Hotbar;
 use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\inventory\InventoryCloseEvent;
+use pocketmine\event\inventory\InventoryEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\network\mcpe\protocol\types\NetworkInventoryAction;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
@@ -18,9 +24,7 @@ use pocketmine\item\Item;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\nbt\tag\ListTag;
 class Hotbar extends PluginBase implements Listener {
-    // world,slot,item,nameOfItem,command,playerOrConsole,glowing
-    public $config = (array("Slots" => array("world,0,339:0,name,true,command,player"), "Cooldown" => 0.5)
-    );
+    public $config = array ('Items' => array ('world' => array (0 => array ('Item' => '399:0:1', 'ItemName' => 'Name', 'Command' => 'Command', 'Executor' => 'Player', 'Enchant' => true,),),),'Locked Inventory' => array("world"),'Cooldown' => 0.5,);
     private $tap;
     public $dataPath;
     public function onEnable(): void {
@@ -30,6 +34,7 @@ class Hotbar extends PluginBase implements Listener {
             $resource = fopen($this->dataPath, 'w') or die("Unable to create hotbar.yml!");
             fwrite($resource, yaml_emit($this->config));
             $this->getLogger()->notice("Successfully created hotbar.yml");
+            fclose($resource);
         }
         $this->readConfig();
     }
@@ -40,87 +45,109 @@ class Hotbar extends PluginBase implements Listener {
         $this->getLogger()->notice("Configuration read successfully");
     }
 
-    public function onJoin(PlayerJoinEvent $event):void{
+    public function onJoin(PlayerJoinEvent $event) :void {
 
         $player = $event->getPlayer();
-        $inv = $player->getInventory();
 
-        foreach ($this->config["Slots"] as $slot){
-            $explosion = explode(",", $slot);
-            if($explosion[0] == $player->getLevel()->getName()){
-                $bang = explode(":", $explosion[2]);
-                $item = Item::get((int)$bang[0], (int)$bang[1], 1);
-                $item->setCustomName($explosion[3]);
-                if($explosion[4] == "true"){
-                    $item->setNamedTagEntry(new ListTag("ench"));
+        $player->getInventory()->clearAll();
+
+        foreach ($this->config['Items'] as $key => $world){
+            if($key == $player->getLevel()->getName()){
+                foreach ($world as $slot => $slotData){
+                    $bang = explode(":", $slotData['Item']);
+                    $item = Item::get((int)$bang[0], (int)$bang[1], (int)$bang[2]);
+                    $item->setCustomName($slotData['ItemName']);
+                    if($slotData['Enchant']){
+                        $item->setNamedTagEntry(new ListTag("ench"));
+                    }
+                    $player->getInventory()->setItem($slot, $item);
                 }
-                $inv->setItem((int)$explosion[1], $item);
             }
         }
     }
 
-    public function onRespawn(PlayerRespawnEvent $event):void{
+    public function onRespawn(PlayerRespawnEvent $event) :void {
 
         $player = $event->getPlayer();
-        $inv = $player->getInventory();
 
-        foreach ($this->config["Slots"] as $slot){
-            $explosion = explode(",", $slot);
-            if($explosion[0] == $player->getLevel()->getName()){
-                $bang = explode(":", $explosion[2]);
-                $item = Item::get((int)$bang[0], (int)$bang[1], 1);
-                $item->setCustomName($explosion[3]);
-                if($explosion[4] == "true"){
-                    $item->setNamedTagEntry(new ListTag("ench"));
+        foreach ($this->config['Items'] as $key => $world){
+            if($key == $player->getLevel()->getName()){
+                foreach ($world as $slot => $slotData){
+                    $bang = explode(":", $slotData['Item']);
+                    $item = Item::get((int)$bang[0], (int)$bang[1], (int)$bang[2]);
+                    $item->setCustomName($slotData['ItemName']);
+                    if($slotData['Enchant']){
+                        $item->setNamedTagEntry(new ListTag("ench"));
+                    }
+                    $player->getInventory()->setItem($slot, $item);
                 }
-                $inv->setItem((int)$explosion[1], $item);
             }
         }
     }
 
-    public function switchWorld(EntityLevelChangeEvent $event):void{
+    public function moveInventory(InventoryTransactionEvent $event) :void {
+
+        $player = $event->getTransaction()->getSource();
+
+        if(in_array($player->getLevel()->getName(), $this->config['Locked Inventory'])){
+            $event->setCancelled();
+        }
+    }
+
+    public function switchWorld(EntityLevelChangeEvent $event) :void {
+
         $player = $event->getEntity();
 
         if(!$player instanceof Player) { return; }
-        $inv = $player->getInventory();
 
         $player->getInventory()->clearAll();
-        foreach ($this->config["Slots"] as $slot){
-            $explosion = explode(",", $slot);
-            if($explosion[0] == $event->getTarget()->getName()){
-                $bang = explode(":", $explosion[2]);
-                $item = Item::get((int)$bang[0], (int)$bang[1], 1);
-                $item->setCustomName($explosion[3]);
-                if($explosion[4] == "true"){
-                    $item->setNamedTagEntry(new ListTag("ench"));
+
+        foreach ($this->config['Items'] as $key => $world){
+            if($key == $event->getTarget()->getName()){
+                foreach ($world as $slot => $slotData){
+                    $bang = explode(":", $slotData['Item']);
+                    $item = Item::get((int)$bang[0], (int)$bang[1], (int)$bang[2]);
+                    $item->setCustomName($slotData['ItemName']);
+                    if($slotData['Enchant']){
+                        $item->setNamedTagEntry(new ListTag("ench"));
+                    }
+                    $player->getInventory()->setItem($slot, $item);
                 }
-                $inv->setItem((int)$explosion[1], $item);
             }
         }
     }
 
-
-    public function onInteract(PlayerInteractEvent $event):void{
+    public function onInteract(PlayerInteractEvent $event) :void {
         $player = $event->getPlayer();
-        if(!isset($this->tap[$player->getName()])){
-            $this->tap[$player->getName()] = microtime(true);
-            foreach ($this->config["Slots"] as $slot) {
-                $explosion = explode(",", $slot);
-                if($player->getInventory()->getItemInHand()->getId() == $explosion[2]){
-                    if($explosion[6] == "console"){
-                        $replaced = str_replace("{PLAYER}", $player->getName(), $explosion[5]);
-                        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),$replaced);
-                        break;
+        if (!isset($this->tap[$player->getName()])) {
+            $this->tap[$player->getName()] = $this->config['Cooldown'];
+            foreach ($this->config['Items'] as $key => $world) {
+                if ($key == $player->getLevel()->getName()) {
+                    foreach ($world as $slot => $slotData) {
+                        $bang = explode(":", $slotData['Item']);
+                        if ($player->getInventory()->getItemInHand()->getId() == $bang[0] && $player->getInventory()->getItemInHand()->getDamage() == $bang[1] && $player->getInventory()->getItemInHand()->getCount() == $bang[2]) {
+                            if (strtolower($slotData['Executor']) == "player") {
+                                $player->getServer()->dispatchCommand($player, $slotData['Command']);
+                                $this->tap[$player->getName()] = microtime(true);
+                                break;
+                            }
+                            if (strtolower($slotData['Executor']) == "console") {
+                                $replaced = str_replace("{PLAYER}", $player->getName(), $slotData['Command']);
+                                $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $replaced);
+                            }
+                            $this->tap[$player->getName()] = microtime(true);
+                            break;
+                        }
                     }
-                    if($explosion[6] == "player"){
-                        $player->getServer()->dispatchCommand($player, $explosion[5]);
-                        break;
-                    }
+                    break;
                 }
             }
         }
-        if(microtime(true) - $this->tap[$player->getName()] <= $this->config["Cooldown"]){
-            return; // Don't unset
+
+        if(isset($this->tap[$player->getName()])){
+            if(microtime(true) - $this->tap[$player->getName()] <= $this->config["Cooldown"]){
+                return; // Don't unset
+            }
         }
         unset($this->tap[$player->getName()]);
     }
