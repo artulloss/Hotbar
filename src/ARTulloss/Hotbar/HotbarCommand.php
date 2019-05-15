f1b938fd21b567c293bd6034184bb0514d258f9f
artulloss <35859316+artulloss@users.noreply.github.com>
2019-05-14 22:05:31 -0400

    oh that didn't update

--- src/ARTulloss/Hotbar/HotbarCommand.php
+++ src/ARTulloss/Hotbar/HotbarCommand.php
@@ -5,16 +5,17 @@
  * Date: 11/21/2018
  * Time: 10:41 AM
  */
-
 declare(strict_types = 1);
-namespace ARTulloss\Hotbar;
 
-use pocketmine\command\Command;
+namespace ARTulloss\Hotbar\Command;
+
+use ARTulloss\Hotbar\Types\HotbarInterface;
 use pocketmine\command\CommandSender;
-use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\command\PluginCommand;
 use pocketmine\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\utils\TextFormat;
+use ARTulloss\Hotbar\Main;
 
 /**
  *  _  _  __ _____ __  __  ___
@@ -22,76 +23,55 @@ use pocketmine\utils\TextFormat;
  * | >< | \/ || | | -< /\ | v /
  * |_||_|\__/ |_| |__/_||_|_|_\
  *
- */
-
-/**
- * Class HotbarCommand
- * @package ARTulloss\Hotbar
  * @author ARTulloss
+ * @link https://github.com/artulloss
  */
-class HotbarCommand extends Command implements PluginIdentifiableCommand {
 
-	/** @var Hotbar $plugin */
-	private $plugin;
+class HotbarCommand extends PluginCommand {
 
-	/**
-	 * HotbarCommand constructor.
-	 * @param $hotbar
-	 */
-	public function __construct(Hotbar $hotbar) {
-		parent::__construct("hotbar", "For making multiple Hotbars on a world!", \null, []);
-		$this->plugin = $hotbar;
-	}
+    /**
+     * HotbarCommand constructor.
+     * @param string $name
+     * @param Plugin $owner
+     */
+    public function __construct(string $name, Plugin $owner) {
+        parent::__construct($name, $owner);
+    }
 
-	/**
-	 * @return Plugin
-	 */
-	public function getPlugin(): Plugin {
-		return $this->plugin;
-	}
+    /**
+     * @param CommandSender $sender
+     * @param string $commandLabel
+     * @param array $args
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void{
 
-	/**
-	 * @param CommandSender $sender
-	 * @param string $commandLabel
-	 * @param array $args
-	 */
-	public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-
-		if($sender instanceof Player) {
-			$sender->sendMessage(TextFormat::BLUE . "Hotbar v" . Hotbar::VERSION . " by ARTulloss");
-			return;
-		}
-
-		if(!isset($args[0]) || !isset($args[1])){
-			$this->plugin->getServer()->getLogger()->notice("Missing an argument");
-			return;
-		}
-
-		$player = $this->getPlugin()->getServer()->getPlayer($args[1]);
-
-		if(!$player instanceof Player){
-			$this->plugin->getServer()->getLogger()->notice("Player not found");
-			return;
-		}
-
-		// Checks for WORLD in /hotbar WORLD {player}  and gives player the items for their current world
-
-		if($args[0] == "WORLD") {
-			$level = $player->getLevel()->getName();
-			$this->plugin->setUsing($player->getName(), $level . ":" . "Worlds");
-			$this->plugin->sendItems($player, "Worlds", $level);
-			return;
-		}
-
-		// Checks for the hotbar
-
-		foreach ($this->plugin->getItems() as $name => $data){
-			if($args[0] == $name) {
-				$this->plugin->setUsing($player->getName(), $name . ":" . "Secondary-Hotbars");
-				$this->plugin->sendItems($player, "Secondary-Hotbars", $name);
-				return;
-			}
-		}
-		$this->getPlugin()->getServer()->getLogger()->notice("Hotbar " . $args[0] . " not found");
-	}
+        if($sender instanceof Player) {
+            $sender->sendMessage(TextFormat::BLUE . 'Hotbar v' . Main::VERSION . ' by ARTulloss');
+        } else {
+            $server = $sender->getServer();
+            if(isset($args[0]) && isset($args[1])) {
+                $player = $server->getPlayer($args[1]);
+                if ($player !== null) {
+                    /** @var Main $plugin */
+                    $plugin = $this->getPlugin();
+                    if ($args[0] === '{world}') {
+                        $level = $player->getLevel();
+                        $hotbarUser = $plugin->getHotbarUsers()->getHotbarFor($player);
+                        $hotbar = $plugin->getHotbarLevels()->getHotbarForLevel($level);
+                        $hotbarUser->setHotbar($hotbar);
+                    } elseif ($args[0] === '{clear}') {
+                        $player->getInventory()->clearAll();
+                        $player->getArmorInventory()->clearAll();
+                        $plugin->getHotbarUsers()->remove($player);
+                    } elseif (($hotbars = $plugin->getHotbars()) && isset($hotbars[$args[0]])) {
+                        /** @var HotbarInterface $hotbar */
+                        $hotbar = $hotbars[$args[0]];
+                        $plugin->getHotbarUsers()->assign($player, $hotbar);
+                    } else
+                        $plugin->getLogger()->notice("Unknown hotbar name $args[0]");
+                } else
+                    $server->getLogger()->notice('Player ' . $args[1] . ' not found!');
+            }
+        }
+    }
 }
